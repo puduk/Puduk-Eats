e1168ae97cc8a76451559cab32a0ffbe8252e056
puduk <demirmert986@gmail.com>
2025-05-16 00:53:06 +0200

    Add change_data.hpp

---
+++ customer/change_data.hpp
@@ -0,0 +1,9 @@
+#ifndef CHANGE_DATA_H
+#define CHANGE_DATA_H
+#include "customer_data.hpp"
+
+void change_data(CustomerData &c);
+void change_address(CustomerData &c);
+void change_number(CustomerData &c);
+
+#endif
