6d7ee8ef9e30d9c72434f4dca953fcb19d9b85c9
puduk <demirmert986@gmail.com>
2025-05-16 00:53:06 +0200

    Add top_up.hpp

---
+++ customer/top_up.hpp
@@ -0,0 +1,10 @@
+#ifndef TOP_UP_H
+#define TOP_UP_H
+#include "customer_data.hpp"
+
+void top_up(CustomerData& c);
+void total_balance_calculator(CustomerData& c);
+void card_save(CustomerData& c);
+void new_card_adder(CustomerData& c);
+
+#endif
