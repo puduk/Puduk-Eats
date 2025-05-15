66e206283c950333d0eb800f871793cb73e5f507
puduk <demirmert986@gmail.com>
2025-05-16 00:53:06 +0200

    Add restaurant_condition.cpp

---
+++ customer/restaurant_condition.cpp
@@ -0,0 +1,40 @@
+#include "restaurant_condition.hpp"
+#include <iostream>
+#include <fstream>
+#include "customer_menu.hpp"
+
+void restaurant_condition(CustomerData& c){
+
+  std::ifstream file( *c.choosen_restaurant +"_statu.txt"  , std::ios::in);
+  if (!file.is_open()) {
+    std::cout << "Unable to load restaurant menu " << '\n';
+    return;
+  }
+
+
+  std::string active_line;
+  std::string found_activity;
+  bool active = true ;
+
+  while (std::getline(file, active_line)) {
+    if(active_line.find(std::to_string(*c.restaurant_chocie) + ". Restaurant Statu : " ) != std::string::npos){
+      found_activity = active_line.substr(active_line.find(": ") + 2);
+      if(found_activity == "CLOSED"){
+        active = false;
+      }
+    }
+  }
+
+
+
+  if(!active){
+    std::cout << "Currently this restaurant is not open" << '\n';
+    return;
+  }
+
+
+
+  customer_menu(c);
+
+}
+
