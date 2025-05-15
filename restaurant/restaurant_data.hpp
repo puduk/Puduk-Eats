ab6e2d445e7cdb8d81f97f42677c0d66eb45d4d0
puduk <demirmert986@gmail.com>
2025-05-16 00:59:05 +0200

    Add restaurant_data.hpp

---
+++ restaurant/restaurant_data.hpp
@@ -0,0 +1,60 @@
+#ifndef RESTAURANT_DATA_H
+#define RESTAURANT_DATA_H
+#include <iostream>
+
+class RestaurantData{
+  public:
+
+    std::string* restaurant_name;
+    std::string* restaurant_password;
+    std::string* restaurant_address;
+    std::string* restaurant_phone_number;
+
+    int* restaurant_amount;
+
+    int* restaurant_menu_choice;
+
+    std::string* product_name;
+    std::string* alergy_test;
+    double* product_price;
+    double* updated_price;
+    int* calories;
+    double* product_porsion;
+    double* updated_porsion;
+
+
+    int* product_id;
+
+    int* remove_choice;
+    int* update_choice;
+    int* update_price_choice;
+
+    std::string* promo_code;
+    double* promo_price;
+    int* promo_id;
+
+    int* needed_item;
+    char* are_sure;
+
+    std::string* item_name;
+    int* item_amount;
+    int* item_id;
+
+    int* stock_choice;
+    int* updated_stock;
+
+    int* active_choice;
+
+
+
+    RestaurantData();
+    ~RestaurantData();
+
+
+
+};
+
+
+
+
+#endif
