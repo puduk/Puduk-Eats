31d7536acce81d642d58c0b2ca1ff1db36d8101d
puduk <demirmert986@gmail.com>
2025-05-16 00:59:54 +0200

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "first/first_data.hpp"
+#include "customer/customer_data.hpp"
+#include "restaurant/restaurant_data.hpp"
+#include "first/first_page.hpp"
+
+int main() {
+
+    FirstData f;
+    CustomerData c;
+    RestaurantData r;
+
+    first_page(f,r,c);
+
+
+}
