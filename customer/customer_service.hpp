ced931f760bab73f7746e163648f11b891dbef80
puduk <demirmert986@gmail.com>
2025-05-16 00:53:06 +0200

    Add customer_service.hpp

---
+++ customer/customer_service.hpp
@@ -0,0 +1,12 @@
+#ifndef CUSTOMER_SERVICE_H
+#define CUSTOMER_SERVICE_H
+#include "customer_data.hpp"
+
+void customer_service(CustomerData &c);
+void number_shower(CustomerData &c);
+void address_shower(CustomerData &c);
+void technique(CustomerData &c);
+void top_up_guide(CustomerData &c);
+void service_info(CustomerData &c);
+
+#endif
