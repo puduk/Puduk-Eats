b7b2a1592b34eaa65637975a68ff67811c83f01f
puduk <demirmert986@gmail.com>
2025-05-16 02:04:48 +0200

    Update first page implementation (first_page.cpp)

--- first/first_page.cpp
+++ first/first_page.cpp
@@ -1,7 +1,16 @@
 #include "first_page.hpp"
 #include <iostream>
+#include "../restaurant/create_account.hpp"
+#include "../restaurant/sign_in.hpp"
+#include "../customer/user_create_account.hpp"
+#include "../customer/user_sign_in.hpp"
+#include "../restaurant/restaurant_data.hpp"
+#include "../customer/customer_data.hpp"
+#include "restaurant_forget.hpp"
+#include "user_forget.hpp"
 
-void first_page(FirstData& f){
+
+void first_page(FirstData& f , RestaurantData& r , CustomerData& c) {
 
   while(true){
     std::cout << "------------------------------------" << '\n';
@@ -11,28 +20,38 @@ void first_page(FirstData& f){
     std::cout << "4 :: for create restaurant account." << '\n';
     std::cout << "5 :: for user password forget." << '\n';
     std::cout << "6 :: for restaurant password forget." << '\n';
+    std::cout << "7 :: for quit..." << '\n';
     std::cout << "------------------------------------" << '\n';
     std::cout << "Your choice: ";
     std::cin >> *f.first_page_choice;
 
     if(std::cin.fail()){
       std::cout << "Please enter a number.";
-      return;
+      return first_page(f , r , c);
     }
 
     switch(*f.first_page_choice){
       case 1:
-        break;
+        user_sign_in(c);
+      break;
       case 2:
-        break;
+        sign_in(r);
+      break;
       case 3:
-        break;
+        user_create_account(c);
+      break;
       case 4:
-        break;
+        restaurant_create_account(r);
+      break;
       case 5:
-        break;
+        user_forget(f);
+      break;
       case 6:
-        break;
+        restaurant_forget(f);
+      break;
+      case 7:
+        std::cout << "Goodbye!" << '\n';
+      exit(0);
       default:
         std::cout << "Invalid choice." << '\n';
     }
