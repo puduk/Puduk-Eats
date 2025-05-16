2b324c52750226bad5da2c27e37be7840944a2f9
puduk <demirmert986@gmail.com>
2025-05-16 02:04:45 +0200

    Update first launch header (first_data.hpp)

--- first/first_data.hpp
+++ first/first_data.hpp
@@ -1,10 +1,13 @@
 #ifndef FIRST_DATA_H
 #define FIRST_DATA_H
+#include <string>
 
 class FirstData {
-  public:
+public:
 
     int* first_page_choice;
+    std::string* username;
+    std::string* restaurant_name;
 
     FirstData();
     ~FirstData();
