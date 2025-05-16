bb133c2396bec9544d70c656a3129ff2b7300a6f
puduk <demirmert986@gmail.com>
2025-05-16 02:04:55 +0200

    Update first page header (first_page.hpp)

--- first/first_page.hpp
+++ first/first_page.hpp
@@ -1,7 +1,10 @@
 #ifndef FIRST_PAGE_H
 #define FIRST_PAGE_H
 #include "first_data.hpp"
+#include "../restaurant/restaurant_data.hpp"
+#include "../customer/customer_data.hpp"
 
-void first_page(FirstData& f);
+void first_page(FirstData& f , RestaurantData& r , CustomerData& c);
+
 
 #endif
